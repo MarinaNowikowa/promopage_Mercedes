60fb2ae05d2f91e1f6e3d122ed7e21be3fb9ffe2
Marina <nowikowamarina0@gmail.com>
2021-09-21 20:28:02 +0300

    init: add first files

---
+++ js/accordion.js
@@ -0,0 +1,16 @@
+const btns = document.querySelectorAll('.feature__link')
+const lists = document.querySelectorAll('.feature-sub')
+
+btns.forEach((btn, index) => {
+  btn.addEventListener('click', () => {
+    btns.forEach((btnItem, idx) => {
+      if (btnItem === btn) {
+        btnItem.classList.toggle('feature__link_active')
+        lists[idx].classList.toggle('hidden')
+      } else {
+        btnItem.classList.remove('feature__link_active')
+        lists[idx].classList.add('hidden')
+      }
+    })
+  })
+})
